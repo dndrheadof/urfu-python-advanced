28e24b723fba9481d4592abc69f98e2bbf60b130
dndrheadof <uleryt@mail.ru>
2023-02-27 22:40:12 +0500

    chore: Убран ненужный print

--- urfu-python-advanced/module2/get_mean_size.py
+++ urfu-python-advanced/module2/get_mean_size.py
@@ -5,7 +5,6 @@ def get_mean_size(data):
     size_sum = 0
     for line in data:
         columns = line.split()
-        print(columns)
         size_sum += int(columns[4])
 
     return size_sum / len(data)
